1b5aa38cbdd764dbbf1169486e9f9a6cde77b17c
YOON <seoung59@gmail.com>
2023-03-05 15:27:00 +0900

    Feat : 고전독서인증 날짜별 신청 현황

--- src/main/java/sejongPromise/backend/infra/sejong/service/SejongClassicCrawlerService.java
+++ src/main/java/sejongPromise/backend/infra/sejong/service/SejongClassicCrawlerService.java
@@ -2,7 +2,9 @@ package sejongPromise.backend.infra.sejong.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import sejongPromise.backend.global.config.qualifier.ChromeAgentWebClient;
 import sejongPromise.backend.infra.sejong.model.SejongAuth;
@@ -16,10 +18,35 @@ public class SejongClassicCrawlerService {
 
     @Value("${sejong.classic.student-info.api-path}")
     private final String INFO_API;
+    @Value("${sejong.classic.schedule-info.api-path}")
+    private final String SCHEDULE_API;
 
     public String crawlStudentCertificationInfo(SejongAuth auth){
         String html = requestStudentCertificationInfo(auth);
         return html;
+    }
+
+    public String crawlScheduleInfo(SejongAuth auth, String date){
+        String html = requestScheduleInfo(auth, date);
+        return html;
+    }
+
+    private String requestScheduleInfo(SejongAuth auth, String date) {
+        String result;
+        String param = String.format("shDate=%s", date);
+        try{
+            result = webClient.post()
+                    .uri(SCHEDULE_API)
+                    .cookies(auth.authCookies())
+                    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                    .body(BodyInserters.fromValue(param))
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+        }catch (Throwable t){
+            throw new RuntimeException(t);
+        }
+        return result;
     }
 
     private String requestStudentCertificationInfo(SejongAuth auth) {
