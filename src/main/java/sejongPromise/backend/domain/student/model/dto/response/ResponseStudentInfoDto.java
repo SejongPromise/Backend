19c309fe2ece14739d764f3a3cb8e9b288a57e82
YOON <seoung59@gmail.com>
2023-03-21 11:53:45 +0900

    feat: 로그인 응답 DTO 생성

---
+++ src/main/java/sejongPromise/backend/domain/student/model/dto/response/ResponseStudentInfoDto.java
@@ -0,0 +1,14 @@
+package sejongPromise.backend.domain.student.model.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ResponseStudentInfoDto {
+    private final Long studentId;
+    private final String major;
+    private final String name;
+    private final Integer semester;
+
+}
