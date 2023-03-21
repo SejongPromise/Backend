02c8cf0c8d3e3a2aec227b3b1594a2420fc493e5
YOON <seoung59@gmail.com>
2023-03-21 12:46:52 +0900

    Feat: BindException Handler

--- src/main/java/sejongPromise/backend/global/error/ControllerAdvisor.java
+++ src/main/java/sejongPromise/backend/global/error/ControllerAdvisor.java
@@ -1,9 +1,12 @@
 package sejongPromise.backend.global.error;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import sejongPromise.backend.global.error.dto.ErrorResponseDto;
 import sejongPromise.backend.global.error.exception.CustomException;
 
@@ -17,5 +20,18 @@ public class ControllerAdvisor {
         log.error("Error occurred in controller advice: [id={}]", dto.getTrackingId());
         return ResponseEntity.status(e.getErrorCode().getStatus()).body(dto);
     }
+
+    @ExceptionHandler
+    protected ResponseEntity<ErrorResponseDto> badParameterException(BindException e){
+        ErrorResponseDto dto = new ErrorResponseDto(e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(dto);
+    }
+
+    @ExceptionHandler
+    protected ResponseEntity<ErrorResponseDto> badParameterException(MethodArgumentTypeMismatchException e){
+        ErrorResponseDto dto = new ErrorResponseDto(e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(dto);
+    }
+
 
 }
