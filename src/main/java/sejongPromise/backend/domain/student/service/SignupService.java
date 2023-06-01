f8d88fbefdb46c1dac45a473d987287074860598
YOON <seoung59@gmail.com>
2023-06-02 00:07:37 +0900

    chore : 오타수정

--- src/main/java/sejongPromise/backend/domain/student/service/SignupService.java
+++ src/main/java/sejongPromise/backend/domain/student/service/SignupService.java
@@ -63,7 +63,7 @@ public class SignupService {
             }
             //관리자에 의해 삭제된 계정이 다시 회원가입 하려는 경우-else if
             if(student.getStudentStatus() == StudentStatus.DeletedByAdmin){
-                throw new CustomException(ErrorCode.INVALID_ACCESS,"관리자에 의해 삭제된 계쩡입니다.");
+                throw new CustomException(ErrorCode.INVALID_ACCESS,"관리자에 의해 삭제된 계정입니다.");
             }
             //탈퇴한 계정이 재가입한 경우
             if(student.getStudentStatus() == StudentStatus.Deleted){
